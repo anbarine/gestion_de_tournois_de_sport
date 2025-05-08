4a8972d63c1287f5037ffff22d1ee3f2efb79ac1
JujuDeFruits <julien.franchet05@gmail.com>
2025-05-08 22:20:49 +0200

    TournamentController update

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Tournament;
+use App\Entity\User;
 use App\Repository\TournamentRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,17 +19,22 @@ class TournamentController extends AbstractController
     public function list(TournamentRepository $tournamentRepository): JsonResponse
     {
         $tournaments = $tournamentRepository->findAll();
-
         return $this->json($tournaments, 200, [], ['groups' => 'tournament:read']);
     }
 
     #[Route('', name: 'create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    public function create(Request $request, EntityManagerInterface $em, UserRepository $userRepository): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
-        if (!$data || !isset($data['tournamentName'], $data['startDate'], $data['endDate'], $data['description'])) {
+        if (!$data || !isset($data['tournamentName'], $data['startDate'], $data['endDate'], $data['description'], $data['organizerId'])) {
             return $this->json(['error' => 'Données invalides ou incomplètes.'], 400);
+        }
+
+        // Récupération de l'organisateur
+        $organizer = $userRepository->find($data['organizerId']);
+        if (!$organizer) {
+            return $this->json(['error' => 'Organisateur non trouvé.'], 404);
         }
 
         $tournament = new Tournament();
@@ -38,6 +45,16 @@ class TournamentController extends AbstractController
         $tournament->setLocation($data['location'] ?? null);
         $tournament->setMaxParticipants($data['maxParticipants'] ?? 16);
         $tournament->setSport($data['sport'] ?? 'Inconnu');
+        $tournament->setOrganizer($organizer);
+
+        // Si un winner est précisé (optionnel)
+        if (isset($data['winnerId'])) {
+            $winner = $userRepository->find($data['winnerId']);
+            if (!$winner) {
+                return $this->json(['error' => 'Vainqueur non trouvé.'], 404);
+            }
+            $tournament->setWinner($winner);
+        }
 
         $em->persist($tournament);
         $em->flush();
@@ -52,7 +69,7 @@ class TournamentController extends AbstractController
     }
 
     #[Route('/{id}', name: 'update', methods: ['PUT'])]
-    public function update(Request $request, Tournament $tournament, EntityManagerInterface $em): JsonResponse
+    public function update(Request $request, Tournament $tournament, EntityManagerInterface $em, UserRepository $userRepository): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
@@ -60,32 +77,27 @@ class TournamentController extends AbstractController
             return $this->json(['error' => 'Données invalides.'], 400);
         }
 
-        if (isset($data['tournamentName'])) {
-            $tournament->setTournamentName($data['tournamentName']);
+        // Mise à jour des champs simples
+        if (isset($data['tournamentName'])) $tournament->setTournamentName($data['tournamentName']);
+        if (isset($data['startDate'])) $tournament->setStartDate(new \DateTime($data['startDate']));
+        if (isset($data['endDate'])) $tournament->setEndDate(new \DateTime($data['endDate']));
+        if (isset($data['description'])) $tournament->setDescription($data['description']);
+        if (isset($data['location'])) $tournament->setLocation($data['location']);
+        if (isset($data['maxParticipants'])) $tournament->setMaxParticipants($data['maxParticipants']);
+        if (isset($data['sport'])) $tournament->setSport($data['sport']);
+
+        // Mise à jour de l'organisateur
+        if (isset($data['organizerId'])) {
+            $organizer = $userRepository->find($data['organizerId']);
+            if (!$organizer) return $this->json(['error' => 'Organisateur non trouvé.'], 404);
+            $tournament->setOrganizer($organizer);
         }
 
-        if (isset($data['startDate'])) {
-            $tournament->setStartDate(new \DateTime($data['startDate']));
-        }
-
-        if (isset($data['endDate'])) {
-            $tournament->setEndDate(new \DateTime($data['endDate']));
-        }
-
-        if (isset($data['description'])) {
-            $tournament->setDescription($data['description']);
-        }
-
-        if (isset($data['location'])) {
-            $tournament->setLocation($data['location']);
-        }
-
-        if (isset($data['maxParticipants'])) {
-            $tournament->setMaxParticipants($data['maxParticipants']);
-        }
-
-        if (isset($data['sport'])) {
-            $tournament->setSport($data['sport']);
+        // Mise à jour du gagnant
+        if (isset($data['winnerId'])) {
+            $winner = $userRepository->find($data['winnerId']);
+            if (!$winner) return $this->json(['error' => 'Vainqueur non trouvé.'], 404);
+            $tournament->setWinner($winner);
         }
 
         $em->flush();
